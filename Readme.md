e878978921d2e2f88ec151d14c38f38af3ce24fb
chakri <chakri.j66@gmail.com>
2024-10-14 21:37:52 +0530

    Final Project Flask

---
+++ Readme.md
@@ -0,0 +1,142 @@
+# Simple Flask Application
+
+  Flask Installation Guide and Docs : https://flask.palletsprojects.com/en/3.0.x/installation/#python-version
+
+  
+  Flask Documentation : https://flask.palletsprojects.com/en/3.0.x/
+
+  flask run --help [ To take help from the flask run commands ]
+
+  flask run -h 0.0.0.0 -p 5001 --debug [ To run the flask app with host , port , debug on mode ]
+
+         (or)
+
+  python app.py
+
+  pip3 freeze [ To see list of softwares and dependencies ]
+
+  pip3 freeze > requirements.txt [ To store the list of softwares into the requirements.txt ]
+
+  pip3 install -r requirements.txt [ To install all softwares and dependencies require for application using requirements file ]
+
+  curl <website-url>
+
+  curl http://127.0.0.1:5001/hello
+
+  curl -I <website-url>
+
+  curl -I http://127.0.0.1:5001/hello
+
+ 
+
+### Code of Url & Routes in Flask
+
+  from flask import Flask, request, make_response
+
+  app = Flask(__name__)
+
+  @app.route('/')
+  def index():
+    return "<h1>Welcome to the Python Flask Application</h1>"
+
+  '''
+  @app.route('/hello', methods=["GET","POST"])
+  def hello():
+    if (request.method == "GET"):
+      return "You made a GET request\n"
+    elif (request.method == "POST"):
+      return "You made a POST request\n"
+    else:
+      return "You will never see this message\n"
+  '''
+
+  @app.route('/hello')
+  def hello():
+    response = make_response('Hello World\n')
+    response.status_code = 202
+    response.headers['content-type'] = 'application/octet-stream'
+    return response
+
+  # url params
+  @app.route('/greet/<name>')
+  def greet(name):
+    return f"<h1>Hello {name}</h1>"
+
+  # url params
+  @app.route('/add/<int:num1>/<int:num2>')
+  def add(num1,num2):
+    return f"<h2>{num1} + {num2} = {num1 + num2}</h2>"
+
+  # url query params
+  @app.route('/handle_url_params')
+  def handle_params():
+    if ('greeting' in request.args.keys() and 'name' in request.args.keys()):
+      greeting = request.args['greeting']
+      name = request.args.get('name')
+      return f'{greeting}, {name}'
+    else:
+      return 'Some parameters are missing'
+
+  if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
+
+## Code for templates rendering & filters, custom filters
+
+  from flask import Flask, render_template, redirect, url_for
+
+  app = Flask(__name__, template_folder='templates')
+
+  @app.route('/')
+  def index():
+    mylist = [10, 20, 30, 40, 50]
+    return render_template('index.html', mylist=mylist)
+
+  @app.route('/home')
+  def home():
+    some_text = "Hello World!"
+    return render_template('home.html', some_text=some_text)
+
+  @app.template_filter('reverse_string')
+  def reverse_string(s):
+    return s[::-1]
+
+  @app.template_filter('repeat')
+  def repeat(s,times=2):
+    return s * times
+
+  @app.template_filter('alternate_case')
+  def alternate_case(s):
+    return ''.join([c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(s)])
+
+  @app.route('/redirect_endpoint')
+  def redirect_endpoint():
+    return redirect(url_for('home'))
+
+
+  if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
+
+
+
+
+## MYSQL DB CLI Commands to Connect Client
+
+   mysql --host=localhost --user=myname --password=password mydb
+   mysql -h localhost -u myname -ppassword mydb
+
+## MYSQL DB Commands
+
+create database flaskdbapp;
+use flaskdbapp;
+create table employees (
+  emp_id int NOT NULL AUTO_INCREMENT,
+  emp_name varchar(255),
+  emp_mail varchar(255) NOT NULL,
+  emp_sal int,
+  emp_city varchar(255),
+  PRIMARY KEY (emp_id)
+);
+select * from employees;
+
---
+++ app.py
@@ -0,0 +1,239 @@
+import os
+import uuid
+import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for, Response, send_from_directory, jsonify, session, make_response, flash
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+app.secret_key = 'SOMECONTENTKEY'
+
+# Database Connectivity
+app.config['MYSQL_HOST'] = 'database-1.c1asso82qaoo.ap-south-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'Mkrishna*672'
+app.config['MYSQL_DB'] = 'flaskdbapp'
+
+mysql = MySQL(app)
+
+background_color = os.environ.get('BACKGROUND_COLOR');
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  if (request.method == "GET"):
+    return render_template('index.html', color=background_color)
+  elif (request.method == "POST"):
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == "neuralnine" and password == "123456":
+      return "Success"
+    else:
+      return "Failure"
+
+@app.route('/employees')
+def employees():
+  # employees=[{'emp_name': "CHAKRAVARTHI", 'emp_mail': "CHAKRI@GMAIL.COM", 'emp_sal': 30000, 'emp_city': "SRIKAKULAM"}]
+  cur = mysql.connection.cursor()
+  cur.execute('''select * from employees''')
+  data = cur.fetchall()
+  cur.close()
+  return render_template('employees.html',employees=data)
+
+@app.route('/add-employee', methods=['GET','POST'])
+def add_employee():
+  if (request.method == 'GET'):
+    return render_template('addEmployee.html')
+  elif (request.method == 'POST'):
+    emp_name = request.form.get('emp_name')
+    emp_mail = request.form.get('emp_mail')
+    emp_sal = request.form.get('emp_sal')
+    emp_city = request.form.get('emp_city')
+
+    cur = mysql.connection.cursor()
+    cur.execute('''insert into employees (emp_name, emp_mail, emp_sal, emp_city) values (%s, %s, %s, %s)''', (emp_name, emp_mail, emp_sal, emp_city))
+    mysql.connection.commit()
+    flash("Employee Data Inserted Successfully!")
+    return redirect(url_for('employees'))
+
+@app.route('/update-employee/<int:emp_id>', methods=['GET','POST'])
+def update_employee(emp_id):
+  if request.method == 'GET':
+    cur = mysql.connection.cursor()
+    cur.execute("select * from employees where emp_id="+str(emp_id))
+    data = cur.fetchall()
+    print(data[0])
+    cur.close()
+    return render_template('updateEmployee.html', emp_id=emp_id, employee=data[0])
+  elif request.method == 'POST':
+    id_data = request.form.get('id')
+    name = request.form.get('emp_name')
+    email = request.form.get('emp_mail')
+    salary = request.form.get('emp_sal')
+    city = request.form.get('emp_city')
+
+    cur = mysql.connection.cursor()
+    cur.execute("""
+    update employees set emp_name=%s, 
+    emp_mail=%s, emp_sal=%s, emp_city=%s
+    where emp_id=%s
+    """, (name, email, salary, city, id_data))
+    mysql.connection.commit()
+    flash("Employee Data Updated Successfully!")
+    return redirect(url_for('employees'))
+
+@app.route('/delete-employee/<int:emp_id>', methods=['GET', 'POST'])
+def delete_employee(emp_id):
+  if (request.method == 'GET'):
+    return render_template('deleteEmployee.html', emp_id=emp_id)
+  elif request.method == 'POST':
+    id_data = request.form.get('id')
+    cur = mysql.connection.cursor()
+    cur.execute("""
+    delete from employees where emp_id=%s
+    """, (id_data))
+    mysql.connection.commit()
+    flash("Employee Data Deleted Successfully!")
+    return redirect(url_for('employees'))
+    
+
+
+
+@app.route('/dashboard')
+def dashboard():
+  return render_template('dashboard.html', message="Dashboard")
+
+@app.route('/set_data')
+def set_data():
+  session['name'] = 'Mike'
+  session['other'] = 'Hello World'
+  return render_template('dashboard.html', message="Session data set.")  
+
+@app.route('/get_data')
+def get_data():
+  if "name" in session.keys() and "other" in session.keys():
+    name = session['name']
+    other = session['other']
+    return render_template('dashboard.html', message=f'Name: {name}, Other: {other}')
+  else:
+    return render_template('dashboard.html', message=f'No session found.')
+
+@app.route('/clear_session')
+def clear_session():
+  session.clear()
+  return render_template('dashboard.html', message='Session Cleared.')
+
+@app.route('/set_cookie')
+def set_cookie():
+  response = make_response(render_template('dashboard.html', message='Cookie set.'))
+  response.set_cookie('cookie_name', 'cookie_value')
+  return response
+
+@app.route('/get_cookie')
+def get_cookie():
+  if "cookie_name" in request.cookies.keys():
+    cookie_value = request.cookies['cookie_name']
+    return render_template('dashboard.html', message=f'Cookie Value: {cookie_value}')
+  else:
+    return render_template('dashboard.html', message=f'No cookie found.')
+
+@app.route('/remove_cookie')
+def remove_cookie():
+  if "cookie_name" in request.cookies.keys():
+    response = make_response(render_template('dashboard.html', message='Cookie Removed.'))
+    response.set_cookie('cookie_name', expires=0)
+    return response
+  else:
+    return render_template('dashboard.html', message=f'No cookie found.')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+  if request.method == 'GET':
+    return render_template('login.html')
+  elif request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username == 'neuralnine' and password == '123456':
+      flash('Successful Login!')
+      return render_template('dashboard.html', message=f'')
+    else:
+      flash('Login Failed!')
+      return render_template('dashboard.html', message=f'')
+
+@app.route('/file_upload', methods=["POST"])
+def file_upload():
+  file = request.files['file']
+
+  if file.content_type == 'text/plain':
+    return file.read().decode()
+  elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file.content_type == 'application/vnd.ms-excel':
+    df = pd.read_excel(file)
+    return df.to_html()
+
+@app.route('/convert_csv', methods=['POST'])
+def convert_csv():
+  file = request.files['file']
+  df = pd.read_excel(file)
+  response = Response(
+    df.to_csv(),
+    mimetype='text/csv',
+    headers={
+      'Content-Disposition': 'attachment; filename=result.csv'
+    }
+  )
+  return response
+
+@app.route('/convert_csv_two', methods=['POST'])
+def convert_csv_two():
+  file = request.files['file']
+
+  df = pd.read_excel(file)
+
+  if not os.path.exists('downloads'):
+    os.makedirs('downloads')
+
+  filename = f'{uuid.uuid4()}.csv'
+  df.to_csv(os.path.join('downloads', filename))
+
+  return render_template('download.html', filename=filename)
+
+@app.route('/download/<filename>')
+def download(filename):
+  return send_from_directory('downloads', filename, download_name='result.csv')
+
+
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+  greeting = request.json['greeting']
+  name = request.json['name']
+
+  with open('file.txt', 'w') as f:
+    f.write(f'{greeting}, {name}')
+  
+  return jsonify({'message': 'Successfully written!'})
+
+@app.route('/home')
+def home():
+  some_text = "Hello World!"
+  return render_template('home.html', some_text=some_text)
+
+@app.template_filter('reverse_string')
+def reverse_string(s):
+  return s[::-1]
+
+@app.template_filter('repeat')
+def repeat(s,times=2):
+  return s * times
+
+@app.template_filter('alternate_case')
+def alternate_case(s):
+  return ''.join([c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(s)])
+
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+  return redirect(url_for('home'))
+
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5001, debug=True)
